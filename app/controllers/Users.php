ff27ed64d3464f081f9a1625367256116bf8f692
pkindalov <panayotkindalov@gmail.com>
2020-01-21 20:42:22 +0200

    Create methods logout and isLoggedIn in User controller. Updated nav to show menu for authenticated and non-authenticated users

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -143,7 +143,7 @@ class Users extends Controller
 
                 if($loggedUser){
                     //Create Session
-                    die('SUCCESS');
+                   $this->createUserSession($loggedUser);
                 } else {
                     $data['password_err'] = 'Email or Password incorrect';
                     $this->view('users/login', $data);
@@ -163,6 +163,29 @@ class Users extends Controller
 
             //Load view
             $this->view('users/login', $data);
+        }
+    }
+
+    public function createUserSession($user){
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('pages/index');
+    }
+
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('users/login');
+    }
+
+    public function isLoggedIn(){
+        if(isset($_SESSION['user_id'])){
+            return true;
+        } else {
+            return false;
         }
     }
 }
